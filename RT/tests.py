967090ec3b181adde166d463d6c866eca3e12bfa
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-08-20 05:35:06 +0200

    Update tests

--- RT/tests.py
+++ RT/tests.py
@@ -7,13 +7,18 @@ class TestRT(unittest.TestCase):
     def setUp(self):
         #get the api key from the environment
         self.api = RT(os.getenv('API_KEY'))
-        self.result = self.api.search('Inception')
 
     def testSearch(self):
+        self.result = self.api.search('Inception')
         self.assertEqual(self.result.name, 'Inception')
 
     def testRatings(self):
+        self.result = self.api.search('Inception')
         self.assertEqual(self.result.critics_score, 86)
+
+    def testMultipleResults(self):
+        self.result = self.api.search('Austin Powers')
+        self.assertTrue(isinstance(self.result, list))
 
 
 if __name__ == '__main__':
